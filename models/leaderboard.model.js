5352f80c85acba125b6710e7188664333b0b6bf4
chun <yanchun.chang5@gmail.com>
2025-06-04 23:36:44 +0200

    Enhance leaderboard functionality: add userType field, update score submission logic, and implement guest score submission

--- models/leaderboard.model.js
+++ models/leaderboard.model.js
@@ -1,35 +1,40 @@
 const mongoose = require('mongoose');
 
 const leaderboardSchema = new mongoose.Schema({
-    nickname: {
-      type: String,
-      required: true,
-      trim: true,
-      maxlength: 30
-    },
-    playerMode: {
-      type: String,
-      enum: ['single', 'multi'],
-      required: true
-    },
-    playMode: {
-      type: String,
-      enum: ['normal', 'countdown'],
-      required: true
-    },
-    level: {
-      type: String,
-      enum: ['easy', 'medium', 'hard', 'expert'],
-      required: true
-    },
-    time: {
-      type: Number,
-      required: true
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
-  });
-  
-  module.exports = mongoose.model('LeaderboardEntry', leaderboardSchema);
+  nickname: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 20
+  },
+  userType: { 
+    type: String,
+    enum: ['guest', 'registered'],
+    required: true
+  },
+  playerMode: {
+    type: String,
+    enum: ['single', 'multi'],
+    required: true
+  },
+  playMode: {
+    type: String,
+    enum: ['normal', 'countdown'],
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['easy', 'medium', 'hard', 'expert'],
+    required: true
+  },
+  time: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('LeaderboardEntry', leaderboardSchema);
--- routes/leaderboard.routes.js
+++ routes/leaderboard.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const leaderboardController = require('../controllers/leaderboard.controller');
+const authMiddleware = require('../middleware/authMiddleware');
 
-// POST → Score speichern
-router.post('/', leaderboardController.submitScore);
+router.post('/', authMiddleware , leaderboardController.submitScore);
+router.post('/guest', leaderboardController.submitScoreGuest);
+router.get('/', leaderboardController.getLeaderBoard);
 
 module.exports = router;
